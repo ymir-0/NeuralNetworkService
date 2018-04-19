841f78ba34ec56920f53b1e92cf92e836ce3e6c0
ymir <mazieres.pierre@gmail.com>
2018-04-19 22:49:41 +0200

    update perceptron & layers

--- neuralnetworkservice/database/perceptron.py
+++ neuralnetworkservice/database/perceptron.py
@@ -8,7 +8,7 @@ connection = connect(host="yggdrasil", port="5433", dbname="neuronnetwork", user
 # layer
 class LayerDB():
     @staticmethod
-    def create(perceptronId, depthIndex, layer):
+    def insertByPerceptronIdAndDepth(perceptronId, depthIndex, layer):
         # cast arrays elements to float
         weights = [[float(__) for __ in _] for _ in layer.weights]
         biases = [float(_) for _ in layer.biases]
@@ -21,7 +21,7 @@ class LayerDB():
         cursor.close()
         pass
     @staticmethod
-    def getAllByPerceptronId(perceptronId):
+    def selectAllByPerceptronId(perceptronId):
         # initialize layers
         layers=list()
         # select all layers
@@ -31,7 +31,7 @@ class LayerDB():
         cursor.execute(statement, parameters)
         # construct each layer
         for attribut in cursor:
-            # create current layer
+            # def insert current layer
             weights = [[float(__) for __ in _] for _ in attribut[0]]
             biases = [float(_) for _ in attribut[1]]
             layer=Layer.constructFromAttributes(weights,biases)
@@ -41,12 +41,29 @@ class LayerDB():
         cursor.close()
         # return
         return layers
+    @staticmethod
+    def updateByPerceptronId(perceptronId, layers):
+        # delete all layers
+        # INFO : we do not want any old layers to remains if there is less layers
+        LayerDB.deleteAllByPerceptronId(perceptronId)
+        # insert each layer
+        for depthIndex, layer in enumerate(layers): LayerDB.insertByPerceptronIdAndDepth(perceptronId, depthIndex, layer)
+    @staticmethod
+    def deleteAllByPerceptronId(perceptronId):
+        # delete all layers
+        statement = "DELETE FROM neuronnetwork.LAYER WHERE ID_PERCEPTRON=%s"
+        parameters = (perceptronId,)
+        cursor = connection.cursor()
+        cursor.execute(statement, parameters)
+        connection.commit()
+        cursor.close()
+        pass
     pass
 # perceptron
 class PerceptronDB():
     # TODO : add a compensation / full rollback system if failure
     @staticmethod
-    def create(perceptron):
+    def insert(perceptron):
         # insert perceptron
         statement = "INSERT INTO neuronnetwork.PERCEPTRON (COMMENTS) VALUES (%s) RETURNING ID"
         parameters = (perceptron.comments,)
@@ -56,12 +73,12 @@ class PerceptronDB():
         connection.commit()
         cursor.close()
         # insert each layer
-        for depthIndex, layer in enumerate(perceptron.layers): LayerDB.create(id, depthIndex, layer)
+        for depthIndex, layer in enumerate(perceptron.layers): LayerDB.insertByPerceptronIdAndDepth(id, depthIndex, layer)
         # set perceptron id
         perceptron.id = id
         pass
     @staticmethod
-    def getById(id):
+    def selectById(id):
         # select perceptron
         statement = "SELECT ID, COMMENTS FROM neuronnetwork.PERCEPTRON WHERE ID=%s"
         parameters = (id,)
@@ -70,9 +87,21 @@ class PerceptronDB():
         attributs = cursor.fetchone()
         cursor.close()
         # select layers
-        layers=LayerDB.getAllByPerceptronId(id)
+        layers=LayerDB.selectAllByPerceptronId(id)
         # construct & return perceptron
         perceptron = Perceptron.constructFromAttributes(attributs[0],layers,attributs[1])
         return perceptron
+    @staticmethod
+    def update(perceptron):
+        # update all layers
+        LayerDB.updateByPerceptronId(perceptron.id, perceptron.layers)
+        # update perceptron
+        statement = "UPDATE neuronnetwork.PERCEPTRON SET COMMENTS=%s WHERE ID=%s"
+        parameters = (perceptron.comments,perceptron.id,)
+        cursor = connection.cursor()
+        cursor.execute(statement, parameters)
+        connection.commit()
+        cursor.close()
+        pass
     pass
 pass
--- test/database/testPerceptron.py
+++ test/database/testPerceptron.py
@@ -8,23 +8,35 @@ from neuralnetworkcommon.perceptron import Perceptron
 from neuralnetworkservice.database.perceptron import PerceptronDB
 # test perceptron
 class testPerceptronDB(TestCase):
-    # test CRUD OK
-    def testCrudOK(self):
-        # initialize random perceptron
+    @staticmethod
+    def randomPerceptron():
         layersNumber = randint(2,12)
         dimensions = [randint(2,100) for _ in range(layersNumber)]
         comments = "".join([choice(ascii_letters) for _ in range(15)])
-        initialPerceptron = Perceptron(dimensions,comments)
+        perceptron = Perceptron(dimensions,comments)
+        return perceptron
+    # test CRUD OK
+    def testCrudOK(self):
+        # initialize random perceptron
+        initialPerceptron = testPerceptronDB.randomPerceptron()
         # precheck
         self.assertFalse(hasattr(initialPerceptron,"id"),"ERROR : perceptron has id")
         # call DB create
-        PerceptronDB.create(initialPerceptron)
+        PerceptronDB.insert(initialPerceptron)
         # check DB create
         self.assertTrue(hasattr(initialPerceptron,"id"),"ERROR : perceptron has no id")
         # call DB get by id
-        fetchedPerceptron = PerceptronDB.getById(initialPerceptron.id)
+        fetchedInsertedPerceptron = PerceptronDB.selectById(initialPerceptron.id)
         # check DB get by id
-        self.assertEqual(initialPerceptron,fetchedPerceptron,"ERROR : perceptrons are different")
+        self.assertEqual(initialPerceptron,fetchedInsertedPerceptron,"ERROR : inserted perceptron does not match")
+        # call DB update
+        newPerceptron = testPerceptronDB.randomPerceptron()
+        newPerceptron.id = initialPerceptron.id
+        PerceptronDB.update(newPerceptron)
+        # check DB update
+        fetchedUpdatedPerceptron = PerceptronDB.selectById(initialPerceptron.id)
+        self.assertNotEqual(fetchedUpdatedPerceptron,fetchedInsertedPerceptron,"ERROR : perceptron not updated")
+        self.assertEqual(fetchedUpdatedPerceptron,newPerceptron,"ERROR : updated perceptron does not match")
         pass
     pass
 pass
