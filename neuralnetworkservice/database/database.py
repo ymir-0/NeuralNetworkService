67ff519a533ca963d626f460739f82353e61e02e
ymir <mazieres.pierre@gmail.com>
2018-04-20 19:08:35 +0200

    explain the absence of SQL query / mapping

--- neuralnetworkservice/database/database.py
+++ neuralnetworkservice/database/database.py
@@ -3,6 +3,12 @@
 from psycopg2 import connect
 from abc import ABC
 # database
+'''
+INFO : I do not use any other SQL library such as
+ - python-sql to construct SQL statement
+ - SQLAlchemy for object mapping
+because it can complexify code without any benefits at this moment
+'''
 class Database(ABC):
     # static values
     CONNECTION = connect(host="yggdrasil", port="5433", dbname="neuronnetwork", user="neuronnetwork", password="neuronnetwork")
