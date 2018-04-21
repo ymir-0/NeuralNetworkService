6dea4491aae6c0f302ff7af189132eb5893314f4
ymir <mazieres.pierre@gmail.com>
2018-04-21 17:23:10 +0200

    send a perceptron through WS

--- neuralnetworkservice/service/perceptron.py
+++ neuralnetworkservice/service/perceptron.py
@@ -1,17 +1,25 @@
 # coding=utf-8
 # import
-from flask import Flask
 from flask_restful import Resource
-from flask_restful.reqparse import RequestParser
-#
-parser = RequestParser()
-parser.add_argument('name', type=str, help='Test')
+from flask import request
+from neuralnetworkcommon.perceptron import Perceptron
+from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonEncoder
+from json import loads
 # global perceptron resource
 class GlobalPerceptron(Resource):
     # generate a random perceptron
     def post(self):
-        args = parser.parse_args()
-        return {'hello': 'world ' + args["name"]}
+        # parse parameters
+        json_data = request.get_json()
+        dimensions = json_data["dimensions"]
+        comments = json_data["comments"]
+        # generate & format random perceptron
+        rawPerceptron = Perceptron.constructRandomFromDimensions(dimensions,comments)
+        dumpedPerceptron = ComplexJsonEncoder.dumpComplexObject(rawPerceptron)
+        # INFO : it is important to load to avoid embedded string
+        loadedPerceptron = loads(dumpedPerceptron)
+        # return
+        return loadedPerceptron
     # get all perceptrons IDs
     #def get(self):
     #    args = RequestParser().parse_args()
--- test/service/testPerceptron.py
+++ test/service/testPerceptron.py
@@ -3,14 +3,24 @@
 # import
 from unittest import TestCase
 from neuralnetworkservice.service.service import application
+from json import dumps
+from random import randint, choice
+from string import ascii_letters
+from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonDecoder
 # test perceptron
 class testPerceptronWS(TestCase):
     APPLICATION = application.test_client()
     # test CRUD OK
     def testGlobalGetOk(self):
+        # randomize layers numbers, dimensions & comments
+        layersNumber = randint(2,12)
+        dimensions = [randint(2,100) for _ in range(layersNumber)]
+        comments = "".join([choice(ascii_letters) for _ in range(15)])
+        # get randomized perceptron
+        response = testPerceptronWS.APPLICATION.post("/perceptron",data=dumps({"dimensions": dimensions,"comments":comments}),content_type='application/json')
+        dumpedPerceptron = response.data.decode()
+        rawPerceptron = ComplexJsonDecoder.loadComplexObject(dumpedPerceptron)
         #
-        kw = {"name": "ymir"}
-        result = testPerceptronWS.APPLICATION.post("/perceptron",data={"name": "ymir"})
         pass
     # utilities
     @classmethod
