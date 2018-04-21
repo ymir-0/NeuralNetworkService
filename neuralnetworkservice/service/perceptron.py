427d8baf52eaaf88ac84ab97917bcd447a199140
ymir <mazieres.pierre@gmail.com>
2018-04-21 19:34:49 +0200

    insert perceptron with WS

--- neuralnetworkservice/service/perceptron.py
+++ neuralnetworkservice/service/perceptron.py
@@ -3,10 +3,12 @@
 from flask_restful import Resource
 from flask import request
 from neuralnetworkcommon.perceptron import Perceptron
-from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonEncoder
-from json import loads
+from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonEncoder, ComplexJsonDecoder
+from json import loads, dumps
+from neuralnetworkservice.database.perceptron import PerceptronDB
 # random perceptron resource
 class RandomPerceptron(Resource):
+    # TODO : delete or make optionnal the comments
     def get(self):
         # parse parameters
         parameters = request.get_json()
@@ -24,16 +26,14 @@ class GlobalPerceptron(Resource):
     # create a perceptron
     def post(self):
         # parse parameters
-        parameters = request.get_json()
-        dimensions = parameters["dimensions"]
-        comments = parameters["comments"]
-        # generate & format random perceptron
-        rawPerceptron = Perceptron.constructRandomFromDimensions(dimensions,comments)
-        dumpedPerceptron = ComplexJsonEncoder.dumpComplexObject(rawPerceptron)
-        # INFO : it is important to load to avoid embedded string
-        loadedPerceptron = loads(dumpedPerceptron)
+        loadedPerceptron = request.get_json()
+        # INFO : it is important to dumps to have requested string type
+        dumpedPerceptron = dumps(loadedPerceptron)
+        rawPerceptron = ComplexJsonDecoder.loadComplexObject(dumpedPerceptron)
+        # insert perceptron
+        PerceptronDB.insert(rawPerceptron)
         # return
-        return loadedPerceptron
+        return rawPerceptron.id
     pass
     # get all perceptrons IDs
     def get(self):
--- test/service/testPerceptron.py
+++ test/service/testPerceptron.py
@@ -6,24 +6,39 @@ from neuralnetworkservice.service.service import application
 from json import dumps
 from random import randint, choice
 from string import ascii_letters
-from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonDecoder
+from pythoncommontools.jsonEncoderDecoder.complexJsonEncoderDecoder import ComplexJsonEncoder, ComplexJsonDecoder
 from neuralnetworkcommon.perceptron import Perceptron
+from json import loads
 # test perceptron
 class testPerceptronWS(TestCase):
     APPLICATION = application.test_client()
-    # test CRUD OK
+    CONTENT_TYPE = "application/json"
+    # test random generation
     def testRandomGetOk(self):
         # randomize layers numbers, dimensions & comments
         layersNumber = randint(2,12)
         dimensions = [randint(2,100) for _ in range(layersNumber)]
         comments = "".join([choice(ascii_letters) for _ in range(15)])
         # get randomized perceptron
-        response = testPerceptronWS.APPLICATION.get("/perceptron/random",data=dumps({"dimensions": dimensions,"comments":comments}),content_type='application/json')
+        response = testPerceptronWS.APPLICATION.get("/perceptron/random",data=dumps({"dimensions": dimensions,"comments":comments}),content_type=testPerceptronWS.CONTENT_TYPE)
         dumpedPerceptron = response.data.decode()
         rawPerceptron = ComplexJsonDecoder.loadComplexObject(dumpedPerceptron)
         # check perceptron
         self.assertEqual(response.status_code,200,"ERROR : response status does not match")
         self.assertTrue(type(rawPerceptron)==Perceptron,"ERROR : perceptron type does not match")
+    # test CRUD OK
+    def testCrudOK(self):
+        # randomize initial perceptron
+        layersNumber = randint(2,12)
+        dimensions = [randint(2,100) for _ in range(layersNumber)]
+        comments = "".join([choice(ascii_letters) for _ in range(15)])
+        rawInitialPerceptron = Perceptron.constructRandomFromDimensions(dimensions,comments)
+        dumpedInitialPerceptron = ComplexJsonEncoder.dumpComplexObject(rawInitialPerceptron)
+        loadedInitialPerceptron = loads(dumpedInitialPerceptron)
+        # create perceptron
+        initialPerceptronId = testPerceptronWS.APPLICATION.post("/perceptron",data=dumpedInitialPerceptron,content_type=testPerceptronWS.CONTENT_TYPE)
+        #
+        pass
     # utilities
     @classmethod
     def setUpClass(cls):
